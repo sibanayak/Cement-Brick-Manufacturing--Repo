1ede1ea802d3317524dda735dd82b5829ad26d28
sibanayak <152395051+sibanayak@users.noreply.github.com>
2025-06-03 17:43:19 +0530

    ok

--- js/script.js
+++ js/script.js
@@ -18,6 +18,13 @@ document.addEventListener("DOMContentLoaded", function () {
       menu.classList.remove("active");
     }
   });
+  
+  const faqItems = document.querySelectorAll('.faq-item');
+    faqItems.forEach(item => {
+      item.querySelector('.faq-question').addEventListener('click', () => {
+        item.classList.toggle('active');
+      });
+    });
 
   const orderForm = document.getElementById('orderForm');
   const fullName = document.getElementById('fullName');
